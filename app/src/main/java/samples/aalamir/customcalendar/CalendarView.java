30bc09eccd42978313091138e7addaad2e9419a1
Ahmed Al-Amir <ahmed.alamir@gmail.com>
2015-06-28 23:25:18 +0200

    Added calendar control code

---
+++ app/src/main/java/samples/aalamir/customcalendar/CalendarView.java
@@ -0,0 +1,16 @@
+package samples.aalamir.customcalendar;
+
+import android.content.Context;
+import android.widget.RelativeLayout;
+
+/**
+ * Created by a7med on 28/06/2015.
+ */
+public class CalendarView extends RelativeLayout
+{
+
+	public CalendarView(Context context)
+	{
+		super(context);
+	}
+}
