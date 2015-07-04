7b681f2231f9c99a740cf49f2d688be012c7e03d
Ahmed Al-Amir <ahmed.alamir@gmail.com>
2015-07-04 09:53:17 +0200

    Added CalendarView init functionality

--- app/src/main/java/samples/aalamir/customcalendar/CalendarView.java
+++ app/src/main/java/samples/aalamir/customcalendar/CalendarView.java
@@ -1,16 +1,75 @@
 package samples.aalamir.customcalendar;
 
 import android.content.Context;
-import android.widget.RelativeLayout;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Created by a7med on 28/06/2015.
  */
-public class CalendarView extends RelativeLayout
+public class CalendarView extends LinearLayout
 {
+	// for logging
+	private static final String LOGTAG = "Calendar View";
+
+	// how many days to show, defaults to six weeks, 42 days
+	private static final int DAYS_COUNT = 42;
+
+	// current displayed month
+	private Calendar currentDate = Calendar.getInstance();
+
+	// internal components
+	private ImageView btnPrev;
+	private ImageView btnNext;
+	private TextView txtDate;
+	private GridView grid;
 
 	public CalendarView(Context context)
 	{
 		super(context);
+		initControl(context);
+	}
+
+	public CalendarView(Context context, AttributeSet attrs)
+	{
+		super(context, attrs);
+		initControl(context);
+	}
+
+	public CalendarView(Context context, AttributeSet attrs, int defStyleAttr)
+	{
+		super(context, attrs, defStyleAttr);
+		initControl(context);
+	}
+
+	/**
+	 * Load control xml layout
+	 */
+	private void initControl(Context context)
+	{
+		LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		inflater.inflate(R.layout.control_calendar, this);
+	}
+
+	@Override
+	protected void onFinishInflate()
+	{
+		super.onFinishInflate();
+
+		// layout is inflated, assign local variables to components
+		btnPrev = (ImageView)findViewById(R.id.calendar_prev_button);
+		btnNext = (ImageView)findViewById(R.id.calendar_next_button);
+		txtDate = (TextView)findViewById(R.id.calendar_date_display);
+		grid = (GridView)findViewById(R.id.calendar_grid);
 	}
 }
