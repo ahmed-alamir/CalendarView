9155a8bc370477f8bd003804c2d43560da845e20
Ahmed Al-Amir <ahmed.alamir@gmail.com>
2015-07-10 17:00:05 +0200

    Show long-pressed day in a Toast

--- app/src/main/java/samples/aalamir/customcalendar/MainActivity.java
+++ app/src/main/java/samples/aalamir/customcalendar/MainActivity.java
@@ -4,7 +4,10 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashSet;
 
@@ -23,6 +26,18 @@ public class MainActivity extends ActionBarActivity
 
 		CalendarView cv = ((CalendarView)findViewById(R.id.calendar_view));
 		cv.updateCalendar(events);
+
+		// assign event handler
+		cv.setEventHandler(new CalendarView.EventHandler()
+		{
+			@Override
+			public void onDayLongPress(Date date)
+			{
+				// show returned day
+				DateFormat df = SimpleDateFormat.getDateInstance();
+				Toast.makeText(MainActivity.this, df.format(date), Toast.LENGTH_SHORT).show();
+			}
+		});
 	}
 
 	@Override
